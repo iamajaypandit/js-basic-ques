4652c1c8beff90f7e14490ee25e7f6d0f38d0a46
iamajaypandit <ap971092@gmail.com>
2026-07-15 20:28:11 +0530

    switch operator

--- day4/day7/app7.js
+++ day4/day7/app7.js
@@ -165,3 +165,66 @@ else if(b>c && b>a){
 else if(c>a && c>b){
     console.log("c");
 }
+let n = 28;
+switch(n==28){
+    case 1:
+    console.log("i am young");
+    break;
+    case 2:
+    console.log("i am not young");
+    break;
+    default:
+    console.log("not valid");
+}
+function BasicCalc(A,B,op) {
+  switch (op){
+    case '+':
+    console.log(A+B);
+    break;
+    case '-':
+    console.log(A-B);
+    break;
+    case '*':
+    console.log(A*B);
+    break;
+    case '/' :
+    console.log(A/B);
+    break;
+    default:
+    console.log("not a operator");
+    
+  }
+   
+}
+  
+var readline = require("readline").createInterface(process.stdin);
+
+let inputArr = [];
+var lineNumber = -1;
+
+readline.on("line", readInputs);
+
+function readInputs(line) {
+  inputArr.push(line);
+  lineNumber++;
+  if (lineNumber == 0) {
+    op=inputArr[0];
+  }
+  
+  if (lineNumber == 1) {
+    logic("s");
+    readline.close();
+  }
+}
+
+function logic(input) {
+ 
+
+    let Arr = inputArr[1].split(" ").map((x) => parseInt(x));
+    let A=Arr[0];
+    let B=Arr[1];
+    (BasicCalc(A,B,op));
+  }
+
+
+
